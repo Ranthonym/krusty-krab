f413a9c9b6087deb0815ff4dd87388e161e38bb8
mithrax <rahul.anthonym@gmail.com>
2020-07-06 15:16:00 -0400

    edit burger css module + render static burger component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import classes from "./BurderIngredient.module.css";
+import classes from "./BurgerIngredient.module.css";
 
 class BurgerIngredient extends Component {
   render() {
