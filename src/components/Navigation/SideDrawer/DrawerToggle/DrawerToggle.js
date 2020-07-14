d65a17f66348f229f170c6733e1d4e6f784bca41
mithrax <rahul.anthonym@gmail.com>
2020-07-13 20:39:11 -0400

    change css module styling for drawer toggle again

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,10 +3,10 @@ import React from "react";
 import classes from "./DrawerToggle.module.css";
 
 const drawerToggle = (props) => (
-  <div onClick={props.clicked}>
-    <div className={classes.ToggleMenu}></div>
-    <div className={classes.ToggleMenu}></div>
-    <div className={classes.ToggleMenu}></div>
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
   </div>
 );
 
