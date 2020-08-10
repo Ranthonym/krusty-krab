57fdeedd3be6949d6c8fa1e83fdc101c8ef96d05
mithrax <rahul.anthonym@gmail.com>
2020-08-10 10:53:55 -0400

    create custom input component + initialize input function with switch case

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const input = (props) => {
+  let inputElement = null;
+
+  switch (props.inputType) {
+    case "input":
+      inputElement = <input />;
+      break;
+    case "textarea":
+      inputElement = <textarea />;
+      break;
+    default:
+      inputElement = <input />;
+  }
+
+  return (
+    <div>
+      <label>{props.label}</label>
+    </div>
+  );
+};
+
+export default input;
