2a63e1dd5dd7d4c14021f32a55c537dd7d0362c6
mithrax <rahul.anthonym@gmail.com>
2020-08-14 13:14:22 -0400

    add dropdown menue for shipping speed in order form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -24,6 +24,17 @@ const input = (props) => {
         />
       );
       break;
+    case "select":
+      inputElement = (
+        <select className={classes.Input} value={props.value}>
+          {props.elementConfig.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
     default:
       inputElement = (
         <input
